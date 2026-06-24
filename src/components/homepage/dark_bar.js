0d06827ab461042579143ab8e713da2acf746f11
ILobogamer19 <manuelgornelasn@gmail.com>
2026-06-24 16:34:44 -0300

    feat(menu): adiciona logout e corrige interacoes no menu hamburguer
    - Adiciona botao de logout.
    - Padroniza o estilo do item 'favoritos' para ficar igual aos demais do menu.
    - Corrige o onClick do carrossel que estava sobrepondo e bloqueando os cliques no menu.

--- src/components/homepage/dark_bar.js
+++ src/components/homepage/dark_bar.js
@@ -1,7 +1,8 @@
 "use client";
 import styles from "./geral.module.css";
 import LikeIcon from "../icons/like";
-import { ConfigIcon } from "../icons/config";
+import ConfigIcon from "../icons/config";
+import LogoutIcon from "../icons/logout";
 import { useState } from "react";
 export default function Dark_bar() {
   const [btnpressed, setBtnPressed] = useState(false);
@@ -34,13 +35,22 @@ export default function Dark_bar() {
             className={styles.secondarymenu}
             style={btnpressed ? { height: "250px" } : { height: "0%" }}
           >
-            <p className={styles.fontsecondmenu}>
+            <p className={styles.fontconfigsecondmenu}>
               {" "}
               <LikeIcon /> Favoritos{" "}
             </p>
             <p className={styles.fontconfigsecondmenu}>
               {" "}
               <ConfigIcon /> Configurações{" "}
+            </p>
+            <p
+              className={styles.fontconfigsecondmenu}
+              onClick={() => {
+                setLogin((prev) => !prev);
+              }}
+            >
+              {" "}
+              <LogoutIcon /> logout{" "}
             </p>
           </div>
           <span></span>
