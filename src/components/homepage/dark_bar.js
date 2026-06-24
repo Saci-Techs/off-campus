d7ed10510912a25d8477f37c00e9b7c5b99738ad
Dampestcrowd <jpsilvadepaula.helo@gmail.com>
2026-06-24 16:04:57 -0300

    feat(auth-ui): adicionar fluxo visual de cadastro/login
    - Adicionado botão de cadastro/login na interface.
    - Implementada verificação visual de autenticação (somente front-end).
    - Ocultado botão de cadastro após autenticação simulada.
    - Liberado acesso ao menu hambúrguer para usuários autenticados.

--- src/components/homepage/dark_bar.js
+++ src/components/homepage/dark_bar.js
@@ -1,26 +1,53 @@
 "use client";
 import styles from "./geral.module.css";
 import LikeIcon from "../icons/like";
-import {ConfigIcon} from "../icons/config";
-import LupaIcon from "../icons/lupa";
+import { ConfigIcon } from "../icons/config";
 import { useState } from "react";
 export default function Dark_bar() {
-    const [btnpressed, setBtnPressed] = useState(false);
-    return (
-        <div className={styles.menuconfig}>
-           <div className={styles.caixapesquisa}>
-             <h1 className={styles.menutxtcolor}> Qual evento você quer ir? </h1>
-             <div className={styles.agrupesquisa}> <input className={styles.inputpesquisa} placeholder="Pesquisar..."/><LupaIcon /></div>
-            </div>
-            <button className={styles.menubtn} onClick={() => {setBtnPressed(!btnpressed)}}>
-                <div className={styles.secondarymenu} style={btnpressed ? {height: "250px"} : {height: "0%"}}>
-                    <p className={styles.fontsecondmenu}> <LikeIcon /> Favoritos </p>
-                    <p className={styles.fontconfigsecondmenu}> <ConfigIcon /> Configurações </p>
-                </div>
-                <span></span>
-                <span></span>
-                <span></span>
-            </button>
+  const [btnpressed, setBtnPressed] = useState(false);
+  const [login, setLogin] = useState(false);
+  return (
+    <div className={styles.menuconfig}>
+      <div className={styles.caixapesquisa}>
+        <h1 className={styles.menutxtcolor}> Qual evento você quer ir? </h1>
+      </div>
+      {!login && (
+        <div className={styles.logindiv}>
+          <button
+            className={styles.loginbtn}
+            onClick={() => {
+              setLogin((prev) => !prev);
+            }}
+          >
+            <p className={styles.logintext}> Cadastro/Login </p>
+          </button>
         </div>
-    );
+      )}
+      {login && (
+        <button
+          className={styles.menubtn}
+          onClick={() => {
+            setBtnPressed(!btnpressed);
+          }}
+        >
+          <div
+            className={styles.secondarymenu}
+            style={btnpressed ? { height: "250px" } : { height: "0%" }}
+          >
+            <p className={styles.fontsecondmenu}>
+              {" "}
+              <LikeIcon /> Favoritos{" "}
+            </p>
+            <p className={styles.fontconfigsecondmenu}>
+              {" "}
+              <ConfigIcon /> Configurações{" "}
+            </p>
+          </div>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      )}
+    </div>
+  );
 }
